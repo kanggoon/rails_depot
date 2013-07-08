fcdfee1c841b771354841a7c2d65f901a2338ea1
jeongjoong.kang <herok3@empal.com>
2013-07-09 07:43:42 +0900

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '169b5d0d995d137eb11c695da18289fabb2fbd1bf3158b850018ddfdbdf5ad307b4c0ec1200421ac1a4cb4436cceb255802463689aeb5702fa16a315008e66c4'
